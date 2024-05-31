ed559f4bf5f54822fc66f974ef8af95e4a9137c6
Denis B. <Deniscapital@mail.ru>
2024-05-31 15:14:58 +0300

    create HW2.0-2.2

---
+++ Lect-Semi2(for,while)/HW2.2.py
@@ -0,0 +1,6 @@
+""" Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N. """
+n = 16
+power_of_two = 1
+while power_of_two <= n:
+    print(power_of_two)
+    power_of_two *= 2
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-print(5)
-print(5)
-print(5)
-print(5)
+""" 
+i = 1
+for color in 'red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'violet':
+    print('#', i, ' color of rainbow is ', color, sep = '')
+    i += 1 """
+
+print(1, 2, 3)
+print(4, 5, 6)
+print(1, 2, 3, sep=', ', end='. ')
+print(4, 5, 6, sep=', ', end='. ') ## ПРИМЕР КАК ОФОРМЛЯТЬ ВЫВОД
+print()
+print(1, 2, 3, sep='', end=' -- ')
+print(4, 5, 6, sep=' * ', end='.')
+
