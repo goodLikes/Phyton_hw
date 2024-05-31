abcbddfebdd883ab6491fe0f634e33993419617d
Denis B. <Deniscapital@mail.ru>
2024-05-31 15:32:52 +0300

    3.0 py

--- main.py
+++ main.py
@@ -4,11 +4,41 @@ for color in 'red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'violet':
     print('#', i, ' color of rainbow is ', color, sep = '')
     i += 1 """
 
-print(1, 2, 3)
+""" print(1, 2, 3)
 print(4, 5, 6)
 print(1, 2, 3, sep=', ', end='. ')
 print(4, 5, 6, sep=', ', end='. ') ## ПРИМЕР КАК ОФОРМЛЯТЬ ВЫВОД
 print()
 print(1, 2, 3, sep='', end=' -- ')
-print(4, 5, 6, sep=' * ', end='.')
+print(4, 5, 6, sep=' * ', end='.') """
+
+""" 
+# ЦИКЛЫ WHILE С ФЛАГОМ США
+first_counter = 0
+
+while first_counter < 5: 
+  print("**********---------")
+  first_counter += 1
+
+second_counter = 0
+
+while second_counter < 4:
+  print("-------------------")
+  second_counter += 1 
+	
+		ЛИБО ТАК 
+		
+		for i in range (4):
+  print("**********---------")
+
+for i in range (4):
+  print("-------------------")
+				"""
+
+""" for i in range(3):
+  print(i)
+  print("For loops are great!") 
+	Считает сколько раз в цикле мы 
+		"""
+
 
