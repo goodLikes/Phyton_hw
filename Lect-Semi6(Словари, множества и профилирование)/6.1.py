7032dec72853afd6bbe0a59ae53ce0154f710029
Denis B. <Deniscapital@mail.ru>
2024-06-10 20:54:54 +0300

    update

--- Lect-Semi6(Словари, множества и профилирование)/6.1.py
+++ Lect-Semi6(Словари, множества и профилирование)/6.1.py
@@ -4,10 +4,10 @@
 # подряд, слова разделены одним или большим числом 
 # пробелов. Определите, сколько различных слов 
 # содержится в этом тексте.
-Input: She sells sea shells on the sea shore The shells
-that she sells are sea shells I'm sure.So if she sells sea
-shells on the sea shore I'm sure that the shells are sea
-shore shells
+# Input: She sells sea shells on the sea shore The shells
+# that she sells are sea shells I'm sure.So if she sells sea
+# shells on the sea shore I'm sure that the shells are sea
+# shore shells
 # Output: 13
 def count_unique_words(text):
     # Разделить текст на слова
